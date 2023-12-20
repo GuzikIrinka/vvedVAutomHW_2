4035ce09ed417e0bdaab99f98462ddb99eb9c90a
GuzikIrinka <106548661+GuzikIrinka@users.noreply.github.com>
2023-12-20 22:29:09 +0300

    Student.java

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,20 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student implements Component{
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println(this.getClass().getName().substring(4) + " " + name);
+    }
+
+    @Override
+    public int getCountPeople() {
+        return 1;
+    }
 }
