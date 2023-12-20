16e5a00126cf12106b5a11463a6655db83c3d632
GuzikIrinka <106548661+GuzikIrinka@users.noreply.github.com>
2023-12-20 22:30:00 +0300

    Teacher.java

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,2 +1,20 @@
-package org.example;public class Teacher {
+package org.example;
+
+public class Teacher implements Component{
+    private String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println(this.getClass().getName().substring(4) + " " + name);
+    }
+
+    @Override
+    public int getCountPeople() {
+        return 1;
+    }
 }
